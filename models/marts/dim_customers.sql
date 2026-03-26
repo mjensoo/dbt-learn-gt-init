3a8ff9abc45f6d1f52cf562ae77a24ecf064d619
jithesh puthiyapurayil <jithesh.puthiyapurayil@dbtlabsworkshop.com>
2026-03-26 20:48:36 +0000

    Training day 2, created Dim customer and staging models

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,27 @@
+{{
+    config(
+        materialized='table',
+        transient=false
+    )
+}}
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
-
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
-
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -25,7 +29,6 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
