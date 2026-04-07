142aed31ff9d1df95d6bfe1f90aefa09873c7e7c
akash dubey <akash.dubey@dbtlabsworkshop.com>
2026-04-07 11:59:20 +0000

    merge day 2 training:added stg models

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -8,23 +8,23 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -32,7 +32,6 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
