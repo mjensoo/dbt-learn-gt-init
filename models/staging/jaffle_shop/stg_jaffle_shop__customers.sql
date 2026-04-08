9e69ad0835332be6947a6bc9bec466389e3eb48e
monika pharnawis <monika.pharnawis@dbtlabsworkshop.com>
2026-04-08 10:00:31 +0000

    merge

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='table') }}
+
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from source
+
+)
+
+select * from renamed
